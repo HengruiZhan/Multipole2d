1b30f09c7aa11e2d3a490509d2a428d3971d7195
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-10-11 22:39:30 -0400

    Update multipole.py

--- multipole.py
+++ multipole.py
@@ -148,3 +148,163 @@ class Multipole():
                                 np.conj(mtilde_i) * R_lm)
 
         return -np.real(phi_zone)
+
+    
+    @jit
+def calcLegPolyL(l, x):
+    # Calculate the Legendre polynomials. We use a stable recurrence relation:
+    # (l+1) P_{l+1}(x) = (2l+1) x P_l(x) - l P_{l-1}(x).
+    # This uses initial conditions:
+    # P_0(x) = 1
+    # P_1(x) = x
+    if (l == 0):
+        legPolyL = 1.0
+    elif(l == 1):
+        legPolyL = x
+    else:
+        legPolyL2 = 1.0
+        legPolyL1 = x
+        for n in range(2, l+1):
+            legPolyL = ((2*n - 1) * x * legPolyL1 - (n-1) * legPolyL2) / n
+            legPolyL2 = legPolyL1
+            legPolyL1 = legPolyL
+    return legPolyL
+
+
+class Multipole2d():
+    """The Multipole Expansion in 2d case"""
+
+    def __init__(self, grid, lmax, dr, density, center=(0.0, 0.0)):
+
+        self.g = grid
+        self.lmax = lmax
+        self.dr = dr
+        self.density = density
+        self.center = center
+
+        self.radius = np.sqrt((self.g.r2d - self.center[0])**2 +
+                              (self.g.z2d - self.center[1])**2)
+        self.cosTheta = self.g.z2d/self.radius
+        self.m = self.density * self.g.vol
+
+        self.phi = self.g.scratch_array()
+
+        # compute the bins
+        # this computation method is not correct
+        r_max = max(abs(self.g.rlim[0] - center[0]), abs(self.g.rlim[1] -
+                                                         center[0]))
+        z_max = max(abs(self.g.zlim[0] - center[1]), abs(self.g.zlim[1] -
+                                                         center[1]))
+
+        # find the r_bins
+        dmax = np.sqrt(r_max**2 + z_max**2)
+
+        self.n_bins = int(dmax/dr)
+
+        self.r_bin = np.linspace(0, dmax, self.n_bins)
+
+    @jit
+    def calcSolHarm(self, l):
+        # calculate the solid harmonic function R_lm and I_lm in
+        # eq 15 and eq 16
+
+        P_l = self.g.scratch_array()
+        self.R_l = self.g.scratch_array()
+        self.I_l = self.g.scratch_array()
+
+        for i in range(self.g.nr):
+            for j in range(self.g.nz):
+                P_l[i, j] = calcLegPolyL(l, self.cosTheta[i, j])
+        self.R_l = self.radius**l*P_l
+        self.I_l = self.radius**(-l-1)*P_l
+
+    @jit
+    def calcML(self):
+        # calculate the outer and inner multipole moment function
+        # M_lm^R and M_lm^I in eq 17 and eq 18
+
+        self.m_r = np.zeros((self.n_bins), dtype=np.complex128)
+        self.m_i = np.zeros((self.n_bins), dtype=np.complex128)
+
+        for i in range(self.n_bins):
+            imask = self.radius <= self.r_bin[i]
+            omask = self.radius > self.r_bin[i]
+            self.m_r[i] += np.sum(self.R_l[imask] * self.m[imask])
+            self.m_i[i] += np.sum(self.I_l[omask] * self.m[omask])
+
+    @jit
+    def sample_mtilde(self, r):
+        # calculate the interpolated multipole moment M_lm^R^tilde
+        # and M_lm^I^tilde in eq 19
+
+        # we need to find which be we are in
+        mu_m = np.argwhere(self.r_bin <= r)[-1][0]
+        mu_p = np.argwhere(self.r_bin > r)[0][0]
+
+        assert mu_p == mu_m + 1
+
+        mtilde_r = (r - self.r_bin[mu_m])/(self.r_bin[mu_p] - self.r_bin[mu_m]
+                                           ) * self.m_r[mu_p] + \
+            (r - self.r_bin[mu_p])/(self.r_bin[mu_m] -
+                                    self.r_bin[mu_p]) * self.m_r[mu_m]
+
+        mtilde_i = (r - self.r_bin[mu_m])/(self.r_bin[mu_p] - self.r_bin[mu_m]
+                                           ) * self.m_i[mu_p] + \
+            (r - self.r_bin[mu_p])/(self.r_bin[mu_m] -
+                                    self.r_bin[mu_p]) * self.m_i[mu_m]
+        return mtilde_r, mtilde_i
+
+    @jit
+    def calcMulFace(self, dr, dz, l):
+        # calculate the contribution of M_lm^R^tilde * conj(I_lm) +
+        # conj(M_lm^I^tilde)*R_lm at the surface
+        # evaluated at the face of the cell
+
+        rFace = self.r2d + dr
+        zFace = self.z2d + dz
+
+        radius = np.sqrt((rFace - self.center[0])**2 +
+                         (zFace - self.center[1])**2)
+
+        cosTheta = zFace/radius
+
+        mtilde_r, mtilde_i = self.sample_mtilde(radius)
+
+        P_l = calcLegPolyL(l, cosTheta)
+        R_l = radius**l * P_l
+        I_l = radius**(-l-1) * P_l
+
+        phiFace_l = mtilde_r * I_l + mtilde_i * R_l
+
+        return phiFace_l
+
+    @ jit
+    def Phi(self):
+
+        dr = self.g.dr
+        dz = self.g.dz
+        area_m_r = 2*np.pi*(self.g.r2d-dr)*dz
+        area_m_z = np.pi*((self.g.r2d+dr)**2 - (self.g.r2d-dr)**2)
+        area_p_r = 2*np.pi*(self.g.r2d+dr)*dz
+        area_p_z = np.pi*((self.g.r2d+dr)**2 - (self.g.r2d-dr)**2)
+        total_area = area_m_r+area_m_z +\
+            area_p_r+area_p_z
+        phi = self.g.scratch_array()
+
+        # for l in range(self.lmax+1):
+        for l in range(0, self.lmax+1, 2):
+            # in case where the mass is symetric, the multipole of odd l vanish
+            self.calcSolHarm(l)
+            self.calcML()
+            MulFace_minus_r = self.calcMulFace(-dr/2, 0)
+            MulFace_minus_z = self.calcMulFace(0, -dz/2)
+            MulFace_plus_r = self.calcMulFace(dr/2, 0)
+            MulFace_plus_z = self.calcMulFace(0, dz/2)
+            phi += (MulFace_minus_r*area_m_r +
+                    MulFace_minus_z*area_m_z +
+                    MulFace_plus_r*area_p_r +
+                    MulFace_plus_z*area_p_z)
+
+        phi = -sc.G*phi/total_area
+
+        return phi
