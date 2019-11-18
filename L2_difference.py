af7c5fd26599799795a90163638ec5ff1b6a1224
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-11-17 22:59:53 -0500

    Update L2_difference.py

--- L2_difference.py
+++ L2_difference.py
@@ -1,9 +1,35 @@
 import numpy as np
 
 
-def L2_diff(analytical_phi, approximate_phi, gcenter):
+def L2_diff(analytical_phi, approximate_phi, grid):
     phi_difference = analytical_phi - approximate_phi
-    N = gcenter.size
-    pot_diff_norm = np.sqrt(np.sum(np.square(phi_difference)/N))
-    # pot_diff_norm = np.sqrt(np.sum(np.square(phi_difference)))
-    return pot_diff_norm
+    '''
+    N = grid.scratch_array().size
+    L2_norm = np.sqrt(np.sum(np.square(phi_difference)/N))
+    analytical_norm = np.sqrt(np.sum(analytical_phi**2/N))
+    '''
+    L2_norm = np.sqrt(np.sum(np.square(phi_difference)))
+    analytical_norm = np.sqrt(np.sum(analytical_phi**2))
+    L2_norm = L2_norm/analytical_norm
+    return L2_norm
+
+
+'''
+def L2_diff(analytical_phi, approximate_phi, grid):
+    phi_difference = analytical_phi - approximate_phi
+    L2_norm = np.sqrt(np.sum(phi_difference**2*grid.vol_project))
+    analytical_norm = np.sqrt(np.sum(analytical_phi**2*grid.vol_project))
+    L2_norm = L2_norm/analytical_norm
+
+    return L2_norm
+    '''
+
+'''
+def L2_diff(analytical_phi, approximate_phi, grid):
+    phi_difference = analytical_phi - approximate_phi
+    L2_norm = np.sqrt(np.sum(phi_difference**2*grid.vol))
+    analytical_norm = np.sqrt(np.sum(analytical_phi**2*grid.vol))
+    L2_norm = L2_norm/analytical_norm
+
+    return L2_norm
+    '''
